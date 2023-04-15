03ee3589dea2a5df2639390c7ba6415588c08919
Maciej Caderek <maciej.caderek@gmail.com>
2023-04-15 19:27:54 +0200

    improved mobile loading

--- src/loadSource.ts
+++ src/loadSource.ts
@@ -4,9 +4,10 @@ const loadSource = (url: string, player: Player) => {
   return new Promise((resolve, reject) => {
     // @ts-ignore
     player.on("error", reject);
-
     // @ts-ignore
     player.on("loadeddata", resolve);
+    // @ts-ignore
+    player.on("loadedmetadata", resolve);
 
     player.src({
       src: url,
